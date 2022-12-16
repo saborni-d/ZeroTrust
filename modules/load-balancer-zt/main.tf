a3bc0a502509dc863892e35f882df7f2179596be
saborni-d <116881883+saborni-d@users.noreply.github.com>
2022-12-16 10:59:51 +0530

    add files

---
+++ modules/load-balancer-zt/main.tf
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+resource "google_iap_client" "iap_client_creds" { #TODO: Authorized Redirect URIs can be done only via console - document this as post terraform step
+  display_name = var.iap_client_name
+  brand        =  var.test_iap_brand_name
+}
---
+++ modules/secret-manager-iap/variables.tf
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "iap_ocs_support_email" {
+  description = "IAP OAuth consent screen support email address"
+  type = string
+}
+variable "iap_ocs_app_title" {
+  description = "IAP OAuth consent screen app title"
+  type = string
+}
+variable "iap_client_name" {
+  description = "IAP OAuth consent screen client id and secret name"
+    type = string
+}
+variable "test_iap_brand_name" {
+  description = "IAP Brand name that was created manually. To list brands use the gcloud command: gcloud iap oauth-brands list --project=PROJECT_ID"
+  type = string
+}
+variable "test_acm_policy_name" {
+  description = "Access Context Manager policy that was created manually. To list acm policy use the gcloud command: gcloud access-context-manager policies list --organization=ORG_ID"
+  type = string
+}
