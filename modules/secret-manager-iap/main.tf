6e43ec685086d8c76b603dd58dbe089dee997b29
saborni-d <116881883+saborni-d@users.noreply.github.com>
2022-12-16 11:01:57 +0530

    Update main.tf

--- modules/secret-manager-iap/main.tf
+++ modules/secret-manager-iap/main.tf
@@ -1,19 +1,24 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-resource "google_iap_client" "iap_client_creds" { #TODO: Authorized Redirect URIs can be done only via console - document this as post terraform step
-  display_name = var.iap_client_name
-  brand        =  var.test_iap_brand_name
+resource "google_secret_manager_secret" "iap_client_id" {
+  secret_id = var.iap_client_id
+  project = google_project_service.enable_project_apis.project
+  replication {
+    automatic = true
+  }
+}
+resource "google_secret_manager_secret" "iap_client_secret" {
+  secret_id = var.iap_client_secret
+  project = google_project_service.enable_project_apis.project
+  replication {
+    automatic = true
+  }
+}
+
+
+resource "google_secret_manager_secret_version" "iap_client_id_version" {
+  secret = google_secret_manager_secret.iap_client_id.id
+  secret_data = google_iap_client.iap_client_creds.client_id
+}
+resource "google_secret_manager_secret_version" "iap_client_secret_version" {
+  secret = google_secret_manager_secret.iap_client_secret.id
+  secret_data = google_iap_client.iap_client_creds.secret
 }
