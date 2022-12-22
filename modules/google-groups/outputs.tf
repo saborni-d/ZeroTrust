5f36783f26fe804186e32167d8858b1b2e80d002
saborni-d <116881883+saborni-d@users.noreply.github.com>
2022-12-22 16:02:29 +0530

    Update outputs.tf

--- modules/google-groups/outputs.tf
+++ modules/google-groups/outputs.tf
@@ -13,3 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+output "id" {
+  value       = google_cloud_identity_group.group.group_key[0].id
+  description = "ID of the group. For Google-managed entities, the ID is the email address the group"
+}
+
+output "resource_name" {
+  value       = google_cloud_identity_group.group.name
+  description = "Resource name of the group in the format: groups/{group_id}, where group_id is the unique ID assigned to the group."
+}
