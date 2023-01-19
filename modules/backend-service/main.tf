8aaea74fb1032f188bf52ab0bcbb24e2350317e9
saborni-d <116881883+saborni-d@users.noreply.github.com>
2023-01-19 18:59:46 +0530

    Add files via upload

--- modules/backend-service/main.tf
+++ modules/backend-service/main.tf
@@ -1,109 +1,15 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-locals {
-  health_check = (
-    var.health_check != null
-    ? var.health_check
-    : try(local.health_check_resource.self_link, null)
-  )
-  health_check_resource = try(
-    google_compute_health_check.http.0,
-    google_compute_health_check.https.0,
-    google_compute_health_check.tcp.0,
-    google_compute_health_check.ssl.0,
-    {}
-  )
-  health_check_type = try(var.health_check_config.type, null)
-}
-
-resource "google_compute_region_backend_service" "default" {
-  provider              = google-beta
+resource "google_compute_backend_service" "backend-service" {
+  name                  = var.backend_service_name
   project               = var.project_id
-  name                  = var.name
-  description           = "Terraform managed."
-  load_balancing_scheme = "INTERNAL"
-  region                = var.region
-  network               = var.network
-  health_checks         = [local.health_check]
-  protocol              = var.protocol
-
-  dynamic "backend" {
-    for_each = { for b in var.backends : b => b }
-    iterator = backend
-    content {
-      group = backend.key
-    }
+  health_checks         = [var.health_check]
+  load_balancing_scheme = "EXTERNAL"            #Global HTTPS LB classic
+  port_name             = var.backend_port_name #"http"
+  protocol              = var.backend_protocol  #"HTTP"
+  backend {
+    group = var.backend_group_url #"https://www.googleapis.com/compute/v1/projects/phonic-sunbeam-372811/zones/asia-south1-c/instanceGroups/instance-group-1"
   }
-
-}
-
-resource "google_compute_health_check" "http" {
-  provider = google-beta
-  count = (
-    var.health_check == null && local.health_check_type == "http" ? 1 : 0
-  )
-  project     = var.project_id
-  name        = var.name
-  description = "Terraform managed."
-
-  http_health_check {
-    port = try(var.health_check_config.port, null)
-  }
-}
-
-resource "google_compute_health_check" "https" {
-  provider = google-beta
-  count = (
-    var.health_check == null && local.health_check_type == "https" ? 1 : 0
-  )
-  project     = var.project_id
-  name        = var.name
-  description = "Terraform managed."
-
-  https_health_check {
-    port = try(var.health_check_config.port, null)
-  }
-}
-
-resource "google_compute_health_check" "tcp" {
-  provider = google-beta
-  count = (
-    var.health_check == null && local.health_check_type == "tcp" ? 1 : 0
-  )
-  project     = var.project_id
-  name        = var.name
-  description = "Terraform managed."
-
-  tcp_health_check {
-    port = try(var.health_check_config.port, null)
-  }
-
-}
-
-resource "google_compute_health_check" "ssl" {
-  provider = google-beta
-  count = (
-    var.health_check == null && local.health_check_type == "ssl" ? 1 : 0
-  )
-  project     = var.project_id
-  name        = var.name
-  description = "Terraform managed."
-
-  ssl_health_check {
-    port = try(var.health_check_config.port, null)
+  iap {
+    oauth2_client_id     = var.oauth2_client_id
+    oauth2_client_secret = var.oauth2_client_secret
   }
 }
--- modules/backend-service/variables.tf
+++ modules/backend-service/variables.tf
@@ -1,83 +1,32 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-variable "address" {
-  description = "Optional IP address used for the forwarding rule."
-  type        = string
-  default     = null
+variable "backend_service_name" {
+  description = "Name of the lb backend service"
+  type = string
 }
-
-variable "global_access" {
-  description = "Global access, defaults to false if not set."
-  type        = bool
-  default     = true //allowing access from all regions as default behaviour
-}
-
-variable "labels" {
-  description = "Labels set on resources."
-  type        = map(string)
-  default     = {}
-}
-
-variable "name" {
-  description = "Name used for all resources."
-  type        = string
-}
-
-variable "network" {
-  description = "Network used for resources."
-  type        = string
-  default = null
-}
-
-variable "ports" {
-  description = "Comma-separated ports, leave null to use all ports."
-  type        = list(string)
-  default     = null
-}
-
 variable "project_id" {
-  description = "Project id where resources will be created."
-  type        = string
+  description = "GCP project where the backend service needs to be created"
+  type = string
 }
-
-variable "protocol" {
-  description = "IP protocol used, defaults to TCP."
-  type        = string
-  default     = "TCP"
+variable "health_check" {
+  description = "Health check for the backend service"
+  type = optional(string)
 }
-
-variable "region" {
-  description = "GCP region."
-  type        = string
+variable "backend_port_name" {
+  description = "Name of port name used in Instance groups"
+  type = string
 }
-
-variable "service_label" {
-  description = "Optional prefix of the fully qualified forwarding rule name."
-  type        = string
-  default     = null
+variable "backend_protocol" {
+  description = "backend service protocol"
+  type = string
 }
-
-variable "subnetwork" {
-  description = "Subnetwork used for the forwarding rule."
-  type        = string
-  default = null
+variable "backend_group_url" {
+  description = "The fully-qualified URL of an Instance Group or Network Endpoint Group resource"
+  type = string
 }
-
-variable "backend_service" {
-  description = "Self link for the backend service associated with the ILB"
-  type        = string
+variable "oauth2_client_id" {
+  description = "OAuth2 Client ID for IAP"
+  type = string
+}
+variable "oauth2_client_secret" {
+  description = "OAuth2 Client Secret for IAP"
+  type = string
 }
